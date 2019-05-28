f7e09fc18953ad11c7bac98d1f2c9633d59c0e6e
Hamed Mortaji <hmortaji@hotmail.com>
2019-05-29 09:19:38 +1000

    Add project files.

---
+++ SevenWestMedia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SevenWestMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
